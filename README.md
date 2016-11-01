97958879dc786255945dc411490bdcedb181821a
KuersatAydinli <kuersat.aydinli@uzh.ch>
2016-11-01 23:39:52 +0100

    added folder structure, update README

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # WebCrawler_Assessment
-This repository is primarily responsible for development of a Python webcrawler which is used to download PDF files from dedicated journal websites
+This repository is primarily responsible for the development of a Python webcrawler which is used to download PDF files from dedicated journal websites,
+namely the 'Journal of Mangement' and the 'American Journal of Sociology'.
 
 # Idea behind Implementation
 For a given journal website, the very first thing to do is to smartly extract links which lead to the desired PDF files.
--- Src/Crawler.py
+++ Src/Crawler.py
@@ -13,14 +13,16 @@ print sample_pdf
 
 #Pring links using urllib2
 print '====== Print Link using URLLIB2 ======'
-website = urllib2.urlopen('http://jom.sagepub.com/content/40/1.toc')
+#website = urllib2.urlopen('http://jom.sagepub.com/content/40/1.toc')
+website = urllib2.urlopen('http://www.journals.uchicago.edu/toc/ajs/2016/121/4')
 html = website.read()
 links = re.findall('"((http|ftp)s?://.*?)"', html)
 print links
 
 print '====== Print Link using BeautifulSoup ======'
 #Print links using BeautifulSoup
-html_site = urllib2.urlopen('http://jom.sagepub.com/content/40/1.toc')
+#html_site = urllib2.urlopen('http://jom.sagepub.com/content/40/1.toc')
+html_site = urllib2.urlopen('http://www.journals.uchicago.edu/toc/ajs/2016/121/4')
 soup = BeautifulSoup(html_site)
 link_list = []
 for link in soup.findAll('a'):
@@ -31,13 +33,13 @@ print link_list
 print '====== Download PDF using urllib2 ======'
 def download_file(download_url):
     response = urllib2.urlopen(download_url)
-    file = open("document.pdf", 'wb')
+    file = open("AmericanJournal2.pdf", 'wb')
     file.write(response.read())
     file.close()
     print("Completed")
 
-download_file('http://jom.sagepub.com/content/40/1/5.full.pdf')
-
+#download_file('http://jom.sagepub.com/content/40/1/5.full.pdf')
+download_file('http://www.journals.uchicago.edu/doi/pdfplus/10.1086/684012.pdf')
 
 
 
