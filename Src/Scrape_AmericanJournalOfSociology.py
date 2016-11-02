cb937c4a8208c507b0a3fadbccc2f5eb7cace4f0
KuersatAydinli <kuersat.aydinli@uzh.ch>
2016-11-02 16:47:47 +0100

    testings with Journal of Sociology

--- Src/Scrape_AmericanJournalOfSociology.py
+++ Src/Scrape_AmericanJournalOfSociology.py
@@ -1,10 +1,19 @@
 # coding=utf-8
 import urllib2
-
+import httplib2
 import lxml.html
-
+from HTMLParser import HTMLParser
+from BeautifulSoup import BeautifulSoup as bs
+import requests
 
 class ScrapeJournalOfSociology():
+    base_urls = {'January': 'http://www.journals.uchicago.edu/toc/ajs/2014/119/4',
+                 'March': 'http://www.journals.uchicago.edu/toc/ajs/2014/119/5',
+                 'May': 'http://www.journals.uchicago.edu/toc/ajs/2014/119/6',
+                 'July': 'http://www.journals.uchicago.edu/toc/ajs/2014/120/1',
+                 'September': 'http://www.journals.uchicago.edu/toc/ajs/2014/120/2',
+                 'November': 'http://www.journals.uchicago.edu/toc/ajs/2014/120/3'}
+
     def download_file(self, download_url, path):
         """
 
@@ -16,4 +25,59 @@ class ScrapeJournalOfSociology():
         file.write(response.read())
         file.close()
         print("Download Complete for " + str(download_url))
+
+
+sample_url = 'http://www.journals.uchicago.edu/toc/ajs/2014/119/4'
+links = lxml.html.fromstring(urllib2.urlopen('http://www.journals.uchicago.edu/toc/ajs/2014/119/4').read()).xpath('//a/@href')
+print links
+#print lxml.html.fromstring(urllib2.urlopen('http://www.journals.uchicago.edu/toc/ajs/2014/119/4').read())
+#print urllib2.urlopen(sample_url).read()
+
+# class MyHTMLParser(HTMLParser):
+#     def handle_starttag(self, tag, attrs):
+#         print "Encountered a start tag:", tag
+#
+#     def handle_endtag(self, tag):
+#         print "Encountered an end tag :", tag
+#
+#     def handle_data(self, data):
+#         print "Encountered some data  :", data
+#
+# parser = MyHTMLParser()
+# parser.feed(urllib2.urlopen(sample_url).read())
+
+# http = httplib2.Http()
+# status, response = http.request(sample_url)
+#
+# for link in BeautifulSoup(response, parseOnlyThese=SoupStrainer('a')):
+#     if link.has_attr('href'):
+#         print link['href']
+
+# connection = urllib2.urlopen(sample_url)
+#
+# dom =  lxml.html.fromstring(connection.read())
+#
+# for link in dom.xpath('//@href'): # select the url in href for all a tags(links)
+#     print link
+
+request = urllib2.Request(sample_url)
+response = urllib2.urlopen(request)
+html = urllib2.urlopen(sample_url).read()
+soup = bs(html)
+linksnew = soup.findAll('a')
+for tag in linksnew:
+    link = tag.get('href',None)
+    if link is not None:
+        print link
+
+print '====================='
+
+url = sample_url
+tree = lxml.html.parse(url)
+#listings = tree.xpath("//a[contains(@href,'/doi/pdfplus')]")
+#listings = tree.xpath("//a[@href='/doi/pdfplus/10.1086/674561']")
+listings = tree.xpath("//a/text()")
+print listings
+
+
 
--- Test/TestAmericanJournalOfSociology.py
+++ Test/TestAmericanJournalOfSociology.py
@@ -2,3 +2,28 @@ import unittest
 import os, os.path
 
 
+# Testclass for testing number of PDFs
+class TestCrawlerJournalOfManagement(unittest.TestCase):
+    def testJanuary(self):
+        Dir = 'F:/Wifo_5_Semester/CrowdSourcing/WebCrawler_Assessment/Src/American Journal of Sociology/January/'
+        self.assertEqual(len([name for name in os.listdir(Dir) if os.path.isfile(os.path.join(Dir, name))]), 32)
+
+    def testMarch(self):
+        Dir = 'F:/Wifo_5_Semester/CrowdSourcing/WebCrawler_Assessment/Src/American Journal of Sociology/March/'
+        self.assertEqual(len([name for name in os.listdir(Dir)]), 36)
+
+    def testMay(self):
+        Dir = 'F:/Wifo_5_Semester/CrowdSourcing/WebCrawler_Assessment/Src/American Journal of Sociology/May/'
+        self.assertEqual(len([name for name in os.listdir(Dir)]), 40)
+
+    def testJuly(self):
+        Dir = 'F:/Wifo_5_Semester/CrowdSourcing/WebCrawler_Assessment/Src/American Journal of Sociology/July/'
+        self.assertEqual(len([name for name in os.listdir(Dir)]), 29)
+
+    def testSeptember(self):
+        Dir = 'F:/Wifo_5_Semester/CrowdSourcing/WebCrawler_Assessment/Src/American Journal of Sociology/September/'
+        self.assertEqual(len([name for name in os.listdir(Dir)]), 29)
+
+    def testNovember(self):
+        Dir = 'F:/Wifo_5_Semester/CrowdSourcing/WebCrawler_Assessment/Src/American Journal of Sociology/November/'
+        self.assertEqual(len([name for name in os.listdir(Dir)]), 30)
