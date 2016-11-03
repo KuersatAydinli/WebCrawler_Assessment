a81d97140a279793ebd6119c435e865f1c37a442
KuersatAydinli <kuersat.aydinli@uzh.ch>
2016-11-03 17:54:31 +0100

    evaluating PDF to Text tools

--- Src/Test.py
+++ Src/Test.py
@@ -1,3 +1,33 @@
-print 'This is a test'
+# Works without space characters
+# import pyPdf
+#
+# print 'This is a test'
+#
+# print 'http://jom.sagepub.com/content/40/2.toc.pdf'[22:] + '+html'
+#
+# testPDF = "FlashTeams.pdf"
+#
+# # with open('FlashTeams.pdf') as f:
+# #     doc = slate.PDF(f)
+# #
+# # print doc
+#
+# pdf = pyPdf.PdfFileReader(open(testPDF, "rb"))
+# for page in pdf.pages:
+#     print page.extractText()
 
-print 'http://jom.sagepub.com/content/40/2.toc.pdf'[22:] + '+html'
+# BUllSHIT
+# import PyPDF2
+#
+# pdf_file = open('FlashTeams.pdf', 'rb')
+# read_pdf = PyPDF2.PdfFileReader(pdf_file)
+# number_of_pages = read_pdf.getNumPages()
+# page = read_pdf.getPage(0)
+# page_content = page.extractText()
+# print page_content
+
+
+# WORKS THE BEST
+# from textract import process
+# text = process('FlashTeams.pdf')
+# print text
