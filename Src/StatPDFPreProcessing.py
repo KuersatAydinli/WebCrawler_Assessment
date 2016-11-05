4915369aa0122b6776da40e69a646f784ca08bbb
KuersatAydinli <kuersat.aydinli@uzh.ch>
2016-11-05 13:07:17 +0100

    added some functions

--- Src/StatPDFPreProcessing.py
+++ Src/StatPDFPreProcessing.py
@@ -12,52 +12,74 @@ t = Table(rows=data_rows, names=('a', 'b', 'c'))
 print(t)
 
 
-# with open('methodlist_full.csv', 'r') as method_file:
-#     for line in method_file.readlines():
-#         print line.split(',')[0]
-
-# function: Initialize table, fill table with journal names
-# function: fill first column row with method names
 class StatPDFPreProcessing:
-
     rootdir = 'F:/Dropbox/Dropbox/all papers'
     stat_method = 'methodlist_full.csv'
-    stat_method_dict = {}
 
-    def getBoolean_per_Method(self, pdf_path, *methods):
-        # Get text of PDF
-        text = process(pdf_path, language='eng')
+    def create_method_bool_dict(self, pdf_path, methods):
+        """
+
+        :rtype: dict : dictionnaire with statistical method as key and boolean for occuring in paper as value
+        :param pdf_path: path to paper
+        :param methods: list of statistical methods specified in 'methodlist_full.csv'
+        """
+        method_synon_dict = self.create_stat_method_dict() # Key: stat. method - Value: acronyms, synonyms
+
+        method_bool_mapping = {}
+        for method in methods:
+            method_bool_mapping[method] = False # Initialize all values to False for all stat. methods
+
+        pdf_text = process(pdf_path, language='eng')
+
+        for key, values in method_synon_dict.iteritems():
+            if len(values) >=1:
+                for value in values:
+                    if value in pdf_text:
+                        method_bool_mapping[key] = True
+
 
     def create_initial_table(self):
-        method_names = []
-        with open(self.stat_method,'r') as method_file:
-            for line in method_file.readlines():
-                method_names.append(line.split(',')[0])
-        # table = Table(data=method_names,names=('stat. Methods'))
+        """
+        Create initial empty Table for final analysis
+        """
+        method_names = self.get_method_names()
         table = Table()
         method_column = Column(name='stat. Methods', data=method_names)
         table.add_column(method_column)
         journal_names = os.listdir(self.rootdir)
         journal_columns = []
-        strs = ["" for x in range(31)]
+        strs = ["" for x in range(31)] # empty string as default entry for table cell
         for journal in journal_names:
             journal_column = Column(name=journal, data=strs)
             journal_columns.append(journal_column)
         table.add_columns(journal_columns)
-        # t = Table(names=('statistical Method',os.listdir(self.rootdir)))
-        # with open('final_analysis.txt', 'w') as final_analysis:
-        #     final_analysis.write(ascii.write(table))
-        print ascii.write(table)
+        #print ascii.write(table, format='fixed_width')
+
+    def get_method_names(self):
+        """
+        Create list of statistical method identifiers
+        """
+        method_names = []
+        with open(self.stat_method, 'r') as method_file:
+            for line in method_file.readlines():
+                method_names.append(line.split(',')[0])
+        return method_names
 
     def create_stat_method_dict(self):
+        """
+        Create dict of statistical method and its synonyms
+        """
         with open(self.stat_method, 'r') as file:
+            stat_method_dict = {}
             for line in file.readlines():
-                self.stat_method_dict[line.split(',')[0]] = line.split(',')[1:]
+                stat_method_dict[line.split(',')[0]] = line.split(',')[1:]
+        return stat_method_dict
+
 
 
 statPreProcessor = StatPDFPreProcessing()
 stat_table = statPreProcessor.create_initial_table()
-# stat_method_dict = statPreProcessor.create_stat_method_dict()
+method_dict = statPreProcessor.create_stat_method_dict()
 
 
 # stat_method_dict = {}
