8bb3e3e31e5ceba61e50d0d85a3a1b0c46cee98c
KuersatAydinli <kuersat.aydinli@uzh.ch>
2016-11-05 11:06:25 +0100

    Testing library for final table generation

--- Src/StatPDFPreProcessing.py
+++ Src/StatPDFPreProcessing.py
@@ -1,12 +1,18 @@
 import os
 from textract import process
+from astropy.table import Table, Column
+import numpy as np
+
+t = Table(names=('a', 'b', 'c'), dtype=('f4', 'i4', 'S2'))
+print t
 
 
-# Hierzu müssten wir für jedes Journal wissen, welche statistische Methode in wie vielen Papers vorkommt.
-# Falls eine Methode mehrmals im gleichen Paper vorkommt, wir sie trotzdem nur einmal gezählt. Beispiel: t-test kommt in Management Science in 23% der Paper vor.
-# Um die Prozentzahl zu kriegen musst du zuerst den Text vom PDF extrahieren, und dann mit regex nach den besagten stat.
-# Methoden suchen. Da bei PDF’s häufig die Leerzeichen etwas fehlerhaft extrahiert werden brauchst du wahrscheinlich regular expressions.
+# with open('methodlist_full.csv', 'r') as method_file:
+#     for line in method_file.readlines():
+#         print line.split(',')[0]
 
+# function: Initialize table, fill table with journal names
+# function: fill first column row with method names
 class StatPDFPreProcessing:
     rootdir = 'F:/Dropbox/Dropbox/all papers'
 
@@ -15,24 +21,24 @@ class StatPDFPreProcessing:
         text = process(pdf_path, language='eng')
 
 
-        # #rootdir = 'F:/Dropbox/Dropbox/WebCrawler_Assessment_PDFs'
-        # rootdir = 'F:/Dropbox/Dropbox/all papers'
-        #
-        # #Add the three Journals also to all papers directory
-        # count = 1
-        # for subdir, dirs, files in os.walk(rootdir):
-        #     for file in files:
-        #         print (os.path.join(subdir, file),count)
-        #         count+=1
+# #rootdir = 'F:/Dropbox/Dropbox/WebCrawler_Assessment_PDFs'
+rootdir = 'F:/Dropbox/Dropbox/all papers'
+#
+# Add the three Journals also to all papers directory
+# count = 1
+# for subdir, dirs, files in os.walk(rootdir):
+#     for file in files:
+#         print (os.path.join(subdir, file),count)
+#         count+=1
 
-        # WORKS THE BEST
-        # from textract import process
-        # text = process('F:/Dropbox/Dropbox/WebCrawler_Assessment_PDFs/Management of Science/June/Doc14.pdf', language='eng')
-        # print '' in text
+# WORKS THE BEST
+# from textract import process
+# text = process('F:/Dropbox/Dropbox/WebCrawler_Assessment_PDFs/Management of Science/June/Doc14.pdf', language='eng')
+# print '' in text
 
-        # with open('methodlist_full.csv', 'r') as file:
-        #     methods = []
-        #     for line in file.readlines():
-        #         for method in line.split(','):
-        #             methods.append(method)
-        # print methods
+# with open('methodlist_full.csv', 'r') as file:
+#     methods = []
+#     for line in file.readlines():
+#         for method in line.split(','):
+#             methods.append(method)
+# print methods
